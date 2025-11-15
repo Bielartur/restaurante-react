aadf5b365b6cfd71a8cc922d04727b2da073eb70
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-11-15 13:09:00 -0300

    Novos componentes de UI

--- src/components/ui/Label.jsx
+++ src/components/ui/Label.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-export function Label({ text, htmlFor, className, children, ...props }) {
+export function Label({ htmlFor, className, children, ...props }) {
 	return (
 		<label className={`flex flex-col gap-1 ${className}`} htmlFor={htmlFor} {...props}>
-            {text}
 			{children}
 		</label>
 	);
